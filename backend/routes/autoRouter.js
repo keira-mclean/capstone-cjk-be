036a1a7c57602785ce1a94be22d69a9349b7610f
Keira-McLean_lmig <keira.mclean@libertymutual.com>
2023-08-23 12:55:23 -0400

    update ppt and add logo

--- backend/routes/autoRouter.js
+++ backend/routes/autoRouter.js
@@ -5,5 +5,6 @@ const autoController = require("../controllers/autoController");
 
 router.get('/', autoController.findAll);
 router.post('/', autoController.post);
+router.findById('/:id', autoController.findByID)
 
 module.exports = router;
