1aa5c62f55d0a15c90e54bc8c35ce6666bb24226
Chris Shade <127913803+cbshade@users.noreply.github.com>
2023-08-24 11:04:22 -0700

    Not great stuff

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,89 +1,94 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Image } from "react-bootstrap";
+import heroImage from '../../src/Hero1.jpg';
+import './Home.css';
+
+
+
 
 export default function Home() {
+
+  const rowStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  };
+
+  const imageStyle = {
+    flex: '1',
+    height: '200px',
+    margin: '0 10px',
+    objectFit: 'cover'
+  };
+
+
   return (
     <>
-      <Container fluid="md">
-        <Row>
-          <header style={{ paddingLeft: 0 }}>
-            <div
-              className="p-5 text-center bg-image"
-              style={{
-                backgroundImage:
-                  "url('https://raw.githubusercontent.com/keira-mclean/capstone-cjk-be/main/frontend/Images/Hero2.jpg')",
-                height: 350,
-                width: 600,
-              }}
-            >
-              <div
-                className="mask"
-                style={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
-              >
-                <div className="d-flex justify-content-center align-items-center h-100">
-                  <div className="text-white">
-                    {/* <h1 className="mb-3">Heading</h1>
-                    <h4 className="mb-3">Subheading</h4> */}
-                    {/* <a
-                      className="btn btn-outline-light btn-lg"
-                      href="#!"
-                      role="button"
-                    >
-                      Call to action
-                    </a> */}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </header>
-          {/* <Col>1 of 1</Col> */}
-        </Row>
-      </Container>
+      <div className="jumboTron container-fluid bg-light p-5" style={{ backgroundImage: `url(${heroImage})` }}>
+        <h1 className="display-4">Jumbotron</h1>
+        <p className="lead">This is a jumbotron created using Bootstrap 5</p>
+        <hr className="my-4" />
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+      </div>
+
+      <p>The Lot is a company that aims to make private party car sales easier and safer for both buyers and sellers. The company provides a platform where people can list their cars for sale, browse through available options, and contact potential sellers. The Lot also offers a range of services to ensure that people get the car they are expecting, such as vehicle inspection, title transfer, financing, and warranty. The Lot’s mission is to simplify the car buying and selling process, while also protecting the interests of both parties.</p>
+      {/* <Container>
+      <Row style={rowStyle}>
+        <Col xs>
+          <Image src="https://raw.githubusercontent.com/keira-mclean/capstone-cjk-be/main/frontend/Images/buycar.jpg" style={imageStyle} />
+        </Col>
+        <Col xs>
+          <Image src="https://raw.githubusercontent.com/keira-mclean/capstone-cjk-be/main/frontend/Images/sellcar.jpg" style={imageStyle} />
+        </Col>
+        <Col xs>
+          <Image src="https://raw.githubusercontent.com/keira-mclean/capstone-cjk-be/main/frontend/Images/ContactUsKiera.jpg" style={imageStyle} />
+        </Col>
+      </Row>
+    </Container> */}
+
+
       <Container>
         <Row>
+          <Col xs>
+            <header style={{ paddingLeft: 0 }}>
+              <div
+                className="homeMenu p-5 text-center bg-image"
+                style={{
+                  backgroundImage:
+                    "url('https://raw.githubusercontent.com/keira-mclean/capstone-cjk-be/main/frontend/Images/buycar.jpg')",
+                  backgroundSize: "cover"
+                }}
+              >
+                <div
+                  className="mask"
+                  style={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+                >
+                  <div className="d-flex justify-content-center align-items-center h-100">
+                    <div className="text-white">
+
+                      <a
+                        className="homeMenuButton btn btn-outline-dark btn-lg"
+                        href="#!"
+                        role="button"
+                      >
+                        Call to action
+                      </a>
+                    </div >
+                  </div >
+                </div >
+              </div >
+            </header >
+          </Col >
           <Col xs>
             {" "}
             <header style={{ paddingLeft: 0 }}>
               <div
-                className="p-5 text-center bg-image"
-                style={{
-                  backgroundImage:
-                    "url('https://raw.githubusercontent.com/keira-mclean/capstone-cjk-be/main/frontend/Images/buycar.jpg')",
-                  height: 350,
-                  width: 600,
-                }}
-              >
-                <div
-                  className="mask"
-                  style={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
-                >
-                  <div className="d-flex justify-content-center align-items-center h-100">
-                    <div className="text-white">
-                      {/* <h1 className="mb-3">Heading</h1>
-                    <h4 className="mb-3">Subheading</h4> */}
-                      <a
-                        className="btn btn-outline-light btn-lg"
-                        href="#!"
-                        role="button"
-                      >
-                        Call to action
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </header>
-          </Col>
-          <Col xs={{ order: 12 }}>
-            {" "}
-            <header style={{ paddingLeft: 0 }}>
-              <div
-                className="p-5 text-center bg-image"
+                className="homeMenu p-5 text-center bg-image"
                 style={{
                   backgroundImage:
                     "url('https://raw.githubusercontent.com/keira-mclean/capstone-cjk-be/main/frontend/Images/sellcar.jpg')",
-                  height: 350,
-                  width: 600,
+                  backgroundSize: "cover"
                 }}
               >
                 <div
@@ -92,10 +97,8 @@ export default function Home() {
                 >
                   <div className="d-flex justify-content-center align-items-center h-100">
                     <div className="text-white">
-                      {/* <h1 className="mb-3">Heading</h1>
-                    <h4 className="mb-3">Subheading</h4> */}
                       <a
-                        className="btn btn-outline-light btn-lg"
+                        className="btn btn-outline-dark btn-lg"
                         href="#!"
                         role="button"
                       >
@@ -107,16 +110,14 @@ export default function Home() {
               </div>
             </header>
           </Col>
-          <Col xs={{ order: 1 }}>
-            {" "}
+          <Col xs>
             <header style={{ paddingLeft: 0 }}>
               <div
-                className="p-5 text-center bg-image"
+                className="homeMenu p-5 text-center bg-image"
                 style={{
                   backgroundImage:
                     "url('https://raw.githubusercontent.com/keira-mclean/capstone-cjk-be/main/frontend/Images/ContactUsKiera.jpg')",
-                  height: 350,
-                  width: 600,
+                  backgroundSize: "cover"
                 }}
               >
                 <div
@@ -124,15 +125,13 @@ export default function Home() {
                   style={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
                 >
                   <div className="d-flex justify-content-center align-items-center h-100">
-                    <div className="text-white">
-                      {/* <h1 className="mb-3">Heading</h1>
-                    <h4 className="mb-3">Subheading</h4> */}
+                    <div className="text-black">
                       <a
-                        className="btn btn-outline-light btn-lg"
+                        className="btn btn-outline-dark btn-lg"
                         href="#!"
                         role="button"
                       >
-                        Call to action
+                        Contact Us
                       </a>
                     </div>
                   </div>
@@ -140,8 +139,8 @@ export default function Home() {
               </div>
             </header>
           </Col>
-        </Row>
-      </Container>
+        </Row >
+      </Container >
     </>
   );
 }
