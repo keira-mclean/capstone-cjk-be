b30f3a49a9e386ceb31e1ff719d0631d749a5f9c
Keira-McLean_lmig <keira.mclean@libertymutual.com>
2023-08-25 08:19:06 -0400

    jumbotron text

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -11,23 +11,29 @@ export default function Home(props) {
   return (
     <>
       <div className="jumboTron container-fluid bg-light p-5" style={{ backgroundImage: `url(${heroImage})` }}>
-        <h1 className="display-4">Jumbotron</h1>
-        <p className="lead">This is a jumbotron created using Bootstrap 5</p>
-        <hr className="my-4" />
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-        <a className="btn btn-primary btn-lg" href="https://www.google.com" role="button">Learn more</a>
+        <h1 className="display-4">Making the Car Buying and Selling Experience easy</h1>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <h5 className="heroText">On the road to happiness</h5>
       </div>
 
-      <p>The Lot is a company that aims to make private party car sales easier and safer for both buyers and sellers. 
-        The company provides a platform where people can list their cars for sale, browse through available options, 
-        and contact potential sellers. The Lot also offers a range of services to ensure that people get the car they 
-        are expecting, such as vehicle inspection, title transfer, financing, and warranty. The Lot’s mission is to 
-        simplify the car buying and selling process, while also protecting the interests of both parties.</p>
-
+      <p className="companyInfo">
+        The Lot is a company that aims to make private party car sales easier and safer for both buyers and sellers.
+        The company provides a platform where people can list their cars for sale, browse through available options,
+        and contact potential sellers. The Lot also offers a range of services to ensure that people get the car they
+        are expecting, such as vehicle inspection, title transfer, financing, and warranty. The Lot’s mission is to
+        simplify the car buying and selling process, while also protecting the interests of both parties.
+      </p>
 
       <Container>
         <Row>
-        <Col xs>
+          <Col xs>
             <header style={{ paddingLeft: 0 }}>
               <div
                 className="homeMenu p-5 text-center bg-image"
