c9260b2812924f486cf24c16b4c8d8da0922e85e
Keira-McLean_lmig <keira.mclean@libertymutual.com>
2023-08-25 08:20:22 -0400

    remove findbyid for now

--- backend/routes/autoRouter.js
+++ backend/routes/autoRouter.js
@@ -5,6 +5,5 @@ const autoController = require("../controllers/autoController");
 
 router.get('/', autoController.findAll);
 router.post('/', autoController.post);
-router.findById('/:id', autoController.findByID)
 
 module.exports = router;
