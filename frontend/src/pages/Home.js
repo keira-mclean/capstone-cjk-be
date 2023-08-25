ad49db01b85fae5b95d40dad3849f176b4e43d82
Keira-McLean_lmig <keira.mclean@libertymutual.com>
2023-08-25 08:03:16 -0400

    add image to frontend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -15,16 +15,20 @@ export default function Home(props) {
         <p className="lead">This is a jumbotron created using Bootstrap 5</p>
         <hr className="my-4" />
         <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-        <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        <a className="btn btn-primary btn-lg" href="https://www.google.com" role="button">Learn more</a>
       </div>
 
-      <p>The Lot is a company that aims to make private party car sales easier and safer for both buyers and sellers. The company provides a platform where people can list their cars for sale, browse through available options, and contact potential sellers. The Lot also offers a range of services to ensure that people get the car they are expecting, such as vehicle inspection, title transfer, financing, and warranty. The Lot’s mission is to simplify the car buying and selling process, while also protecting the interests of both parties.</p>
+      <p>The Lot is a company that aims to make private party car sales easier and safer for both buyers and sellers. 
+        The company provides a platform where people can list their cars for sale, browse through available options, 
+        and contact potential sellers. The Lot also offers a range of services to ensure that people get the car they 
+        are expecting, such as vehicle inspection, title transfer, financing, and warranty. The Lot’s mission is to 
+        simplify the car buying and selling process, while also protecting the interests of both parties.</p>
 
 
       <Container>
         <Row>
-          <Col xs>
-            <header >
+        <Col xs>
+            <header style={{ paddingLeft: 0 }}>
               <div
                 className="homeMenu p-5 text-center bg-image"
                 style={{
@@ -41,15 +45,15 @@ export default function Home(props) {
                     <div className="text-black">
                       <a
                         className="btn btn-outline-dark btn-lg"
-                        href="#!" onClick={(event) => { props.setPage("./pages/Home"); event.preventDefault(); }} role="button">
-                        Search for your new Vehicle
+                        href="https://www.google.com" onClick={(event) => { props.setPage("Buy"); event.preventDefault(); }} role="button">
+                        Find your vehicle
                       </a>
                     </div>
                   </div>
                 </div>
               </div>
-            </header >
-          </Col >
+            </header>
+          </Col>
           <Col xs>
             <header style={{ paddingLeft: 0 }}>
               <div
