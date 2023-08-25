13ea5e7d0d428b251ba3d67109f08b3e0e95a472
Keira-McLean_lmig <keira.mclean@libertymutual.com>
2023-08-25 12:39:01 -0400

    home style and contact form

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import "./Contact.css";
 
-export default function ContactUs(props) {
-    return (
-        <>
-        <h1> contact us </h1>
-        </>
-    )
+const Contact = () => {
+  const [formStatus, setFormStatus] = React.useState('Send')
+  const onSubmit = (e) => {
+    e.preventDefault()
+    setFormStatus('Submited')
+    const { name, email, message } = e.target.elements
+    let conFom = {
+      name: name.value,
+      email: email.value,
+      message: message.value,
+    }
+    console.log(conFom)
+  }
+  return (
+    <div className="contactDiv">
+      <h2 className="mb-3">Contact Us</h2>
+      <form onSubmit={onSubmit}>
+        <div className="mb-3">
+          <label className="contactLabel" htmlFor="name">
+            Name
+          </label>
+          <input className="contactInput" type="text" id="name" required />
+        </div>
+        <div className="mb-3">
+          <label className="contactLabel" htmlFor="email">
+            Email
+          </label>
+          <input className="contactInput" type="email" id="email" required />
+        </div>
+        <div className="mb-3">
+          <label className="contactLabel" htmlFor="message">
+            Message
+          </label>
+          <textarea className="contactInput" id="message" required />
+        </div>
+        <button className="contactButton" type="submit">
+          {formStatus}
+        </button>
+      </form>
+    </div>
+    
+  )
 }
+export default Contact
