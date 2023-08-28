9a4620f8a3ab6b04b6c6f6e44f631644862d9a14
Keira-McLean_lmig <keira.mclean@libertymutual.com>
2023-08-28 11:12:41 -0400

    add sidebar to buy page

---
+++ frontend/src/VehicleFilter.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './VehicleFilter.css';
+
+
+export default function (VehicleFilter) {
+
+    return (
+        <>
+        <div >
+            <p>This is the 1sidebar</p>
+            <p>This is the 2sidebar</p>
+            <p>This is the 3sidebar</p>
+            <p>This is the 4sidebar</p>
+            <p>This is the 5sidebar</p>
+            <p>This is the 6sidebar</p>
+            <p>This is the 7sidebar</p>
+            <p>This is the 8sidebar</p>
+        </div>
+        
+        
+        </>
+    )
+}
+
--- frontend/src/pages/Buy.js
+++ frontend/src/pages/Buy.js
@@ -1,17 +1,23 @@
 import React from "react";
 import './Buy.css';
-import Vehicle from "../Vehicle";
 import VehicleList from "./VehicleList";
+import VehicleFilter from "../VehicleFilter";
 
 export default function Buy(props) {
     return (
         <>
 
             <h3 className="hero-subtext"></h3>
-            <div>
-                <VehicleList />
-                <Vehicle />
+            <div className="body">
+                <div className="sidebar">
+                    <VehicleFilter />
+                </div>
+                <div className="main">
+                    <VehicleList />
+                </div>
             </div>
+
+
 
 
         </>
