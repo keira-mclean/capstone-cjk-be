51e2c4692c9d71010edc8f7fb2107773f59763a4
Keira-McLean_lmig <keira.mclean@libertymutual.com>
2023-08-29 11:30:34 -0400

    push form styling

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -26,22 +26,13 @@ const Contact = () => {
         <h2 className="mb-3">Contact Us</h2>
         <form onSubmit={onSubmit}>
           <div className="mb-3">
-            <label className="contactLabel" htmlFor="name">
-              Name
-            </label>
-            <input className="contactInput" type="text" id="name" required />
+            <input placeholder="Name" className="contactInput" type="text" id="name" required />
           </div>
           <div className="mb-3">
-            <label className="contactLabel" htmlFor="email">
-              Email
-            </label>
-            <input className="contactInput" type="email" id="email" required />
+            <input placeholder="Email" className="contactInput" type="email" id="email" required />
           </div>
           <div className="mb-3">
-            <label className="contactLabel" htmlFor="message">
-              Message
-            </label>
-            <textarea className="contactInput" id="message" required />
+            <textarea placeholder="Message" className="contactInput" id="message" required />
           </div>
           <button className="contactButton" type="submit">
             {formStatus}
