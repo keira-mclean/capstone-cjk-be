b322f3516eb0d6e3b64ae7068f44c738c266699e
Chris Shade <127913803+cbshade@users.noreply.github.com>
2023-08-29 10:04:55 -0700

    Active tab improvements

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,34 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 export default function Navbar(props) {
+    const { page, setPage } = props;
+    const [tab, setTab] = useState("Home");
 
     return (
-
         <nav>
             <img src="https://github.com/keira-mclean/capstone-cjk-be/blob/main/frontend/public/logo2.jpg?raw=true" alt="Logo"></img>
             <ul>
                 <li>
-                    <a href="https://www.google.com" onClick={(event) => { props.setPage("Home"); event.preventDefault(); }}>
+                    <a href="https://www.google.com" onClick={(event) => { setPage("Home"); setTab("Home"); event.preventDefault(); }} className={tab === "Home" ? "active" : ""}>
                         Home
                     </a>
                 </li>
                 <li>
-                    <a href="https://www.google.com" onClick={(event) => { props.setPage("Buy"); event.preventDefault(); }}>
+                    <a href="https://www.google.com" onClick={(event) => { setPage("Buy"); setTab("Buy"); event.preventDefault(); }} className={tab === "Buy" ? "active" : ""}>
                         Buy
                     </a>
                 </li>
                 <li>
-                    <a href="https://www.google.com" onClick={(event) => { props.setPage("Sell"); event.preventDefault(); }}>
+                    <a href="https://www.google.com" onClick={(event) => { setPage("Sell"); setTab("Sell"); event.preventDefault(); }} className={tab === "Sell" ? "active" : ""}>
                         Sell
                     </a>
                 </li>
                 <li>
-                    <a href="https://www.google.com" onClick={(event) => { props.setPage("Contact"); event.preventDefault(); }}>
+                    <a href="https://www.google.com" onClick={(event) => { setPage("Contact"); setTab("Contact"); event.preventDefault(); }} className={tab === "Contact" ? "active" : ""}>
                         Contact Us
                     </a>
                 </li>
             </ul>
         </nav>
-
     )
 };
